85b3cc96d40bb105cee50e50f723c1a6134c72ac
nicoSaffarano1995 <nico.saffarano@gmail.com>
2023-06-14 15:39:15 -0300

    login rendered

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,48 +1,66 @@
-import React from 'react'
-import { useState } from 'react'
-import validation from './validation.js'
+import React from "react";
+import { useState } from "react";
+import validation from "./validation.js";
+import style from "./Login.module.css";
+import { Button, Form } from "react-bootstrap";
 
-const Login = ({login}) => {
-  const [errors, setErrors] = useState({})
+const Login = ({ login }) => {
+  const [errors, setErrors] = useState({});
   const [userData, setUserData] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
 
   const handleChange = (event) => {
     setUserData({
       ...userData,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
-    setErrors(validation({
-      ...userData,
-      [event.target.name]: event.target.value
-    }));
-  }
+    setErrors(
+      validation({
+        ...userData,
+        [event.target.name]: event.target.value,
+      })
+    );
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
     login(userData);
-  }
+  };
   return (
     <div>
       {/* faltan estilos */}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='email'>E-MAIL: </label>
-        <input type='email' name='email' placeholder='Ingresa tu email' value={userData.email} onChange={handleChange}/>
+      <Form onSubmit={handleSubmit} className={style.login}>
+        <label htmlFor="email">E-MAIL: </label>
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Ingresa tu email"
+          value={userData.email}
+          onChange={handleChange}
+        />
         {errors.email && <p>{errors.email}</p>}
-        <label htmlFor='password'>CONTRASEÑA: </label>
-        <input type='password' name='password' placeholder='Ingresa tu contraseña' value={userData.password} onChange={handleChange}/>
+        <label htmlFor="password">CONTRASEÑA: </label>
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="Ingresa tu contraseña"
+          value={userData.password}
+          onChange={handleChange}
+        />
         {errors.password && <p>{errors.password}</p>}
         <div>
-          <button>LOGIN</button>
+          <Button variant="primary" className={style.button}>
+            LOGIN
+          </Button>
         </div>
         <div>
           <h2>¿OLVIDASTE TU CONTRASEÑA?</h2>
           <h3>¿NO TIENES UNA CUENTA? REGISTRATE</h3>
         </div>
-      </form>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,11 +1,13 @@
 import logo from "../../assets/logo.svg";
 import shape from "../../assets/landing/shape.svg";
 import style from "./Landing.module.css";
+import Login from '../../components/Login/Login'
 
 const Landing = () => {
   return (
     <div className={style.mainContainer}>
       <img src={shape} alt="" className={style.shape} />
+      <Login/>
       <img src={logo} alt="logo" className={style.logo}/>
     </div>
   );
