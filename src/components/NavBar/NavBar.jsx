dec2bfa9fa3f81c3b2f416fef9a9edd565ff2792
mateomohr02 <mohr.mateo@gmail.com>
2023-06-15 15:58:40 -0300

    fixes navbar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,8 +9,8 @@ import SearchBar from '../SearchBar/SearchBar';
 const Navbar = () => {
   return (
     <div className={style.navContainer}>
-        <Link className={style.links} to='/home' ><img className={style.logo} src={logo} alt="Logo/Home"/></Link>
       <div className={style.containerLeft} >
+        <Link className={style.links} to='/home' ><img className={style.img} src={logo} alt="Logo/Home"/></Link>
         <Link className={style.links} to='/competitions'>Competitions</Link>
         <Link className={style.links} to='/community'>Community</Link>
       </div>
