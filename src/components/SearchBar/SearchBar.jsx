ed52f34f6644109e93f5d884e92e13a2a765e20b
mateomohr02 <mohr.mateo@gmail.com>
2023-06-14 22:37:08 -0300

    redux states added / searchbar done

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './SearchBar.module.css'
 import lupa from '../../assets/lupa.png'
+import { useDispatch }from 'react-redux'
+//crear action
+
 
 const SearchBar = () => {
+
+  const dispatch = useDispatch();
+
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.targer.value)
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch()
+  }
+
   return (
     <div className={style.searchBar}>
-    <input type="text" className={style.searchInput} placeholder="Buscar equipo/liga..." />
-    <button className={style.searchButton}>
+    <input value={search}type="text" onChange={handleChange} className={style.searchInput} placeholder="Buscar equipo/liga..." />
+    <button onClick={handleSubmit} className={style.searchButton}>
       <img className={style.img} src={lupa} alt="Lupa" />
     </button>
   </div>
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,9 +1,20 @@
 const initialState = {
-
+    nations : [],
+    leagues : [],
+    teams: [],
+    showedLeagues: [],
+    showedTeams : [],
+    
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+
+        case 'SEARCH_TEAM_LEAGUE':
+            return {...state,
+                showedLeagues: action.payload
+            }
+
         default:
             return state;
     }
