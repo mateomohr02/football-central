05dde01d235ffca9778e44340ca5edb04f2faf8e
nicoSaffarano1995 <nico.saffarano@gmail.com>
2023-06-15 18:18:51 -0300

    navbar styles fixed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import logo from '../../assets/logo.svg'
-import { Link } from 'react-router-dom';
-import style from './NavBar.module.css'
-import usuario from  '../../assets/usuario.png'
+import React from "react";
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import style from "./NavBar.module.css";
 
-import SearchBar from '../SearchBar/SearchBar';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
   return (
     <div className={style.navContainer}>
-        <Link className={style.links} to='/home' ><img className={style.logo} src={logo} alt="Logo/Home"/></Link>
-      <div className={style.containerLeft} >
-        <Link className={style.links} to='/competitions'>COMPETITIONS</Link>
-        <Link className={style.links} to='/community'>Community</Link>
+      <Link className={style.links} to="/home">
+        <img className={style.logo} src={logo} alt="Logo/Home" />
+      </Link>
+      <div className={style.containerLeft}>
+        <Link className={style.links} to="/competitions">
+          COMPETITIONS
+        </Link>
+        <Link className={style.links} to="/community">
+          COMMUNITY
+        </Link>
       </div>
       <div className={style.containerSearchAndUser}>
-        <SearchBar/>
-        
-        <Link to='/'><img className={style.user} src={usuario} alt="Logo User"/></Link>
+        <SearchBar />
 
+        <Link to="/" className={style.user}>
+          <AccountCircleIcon fontSize="large" />
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,33 +1,37 @@
-import React, { useState } from 'react'
-import style from './SearchBar.module.css'
-import lupa from '../../assets/lupa.png'
-import { useDispatch }from 'react-redux'
+import React, { useState } from "react";
+import style from "./SearchBar.module.css";
+import { useDispatch } from "react-redux";
+import SearchIcon from "@mui/icons-material/Search";
 //crear action
 
-
 const SearchBar = () => {
-
   const dispatch = useDispatch();
 
   const [search, setSearch] = useState("");
 
   const handleChange = (e) => {
-    setSearch(e.target.value)
-  }
-  
+    setSearch(e.target.value);
+  };
+
   const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch()
-  }
+    e.preventDefault();
+    dispatch();
+  };
 
   return (
     <div className={style.searchBar}>
-    <input value={search}type="text" onChange={handleChange} className={style.searchInput} placeholder="Buscar equipo/liga..." />
-    <button onClick={handleSubmit} className={style.searchButton}>
-      <img className={style.img} src={lupa} alt="Lupa" />
-    </button>
-  </div>
-  )
-}
+      <input
+        value={search}
+        type="text"
+        onChange={handleChange}
+        className={style.searchInput}
+        placeholder="Buscar equipo/liga..."
+      />
+      <button onClick={handleSubmit} className={style.searchButton}>
+        <SearchIcon/>
+      </button>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
