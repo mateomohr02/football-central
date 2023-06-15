f3997917d7283ec6a78b0143e79f8e2a33f56808
mateomohr02 <mohr.mateo@gmail.com>
2023-06-14 22:58:36 -0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Provider } from 'react-redux';
+
+import store from './redux/store/store.js';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
