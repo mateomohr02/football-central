aa7d71abf7feec923ed0a5dafedbe8f11566e869
nicoSaffarano1995 <nico.saffarano@gmail.com>
2023-06-15 00:07:01 -0300

    bootstrap delete

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import store from "./redux/store/store.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
   <Provider store={store}>
-
-  <BrowserRouter>
-
-      <App/>
-
-  </BrowserRouter>
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
