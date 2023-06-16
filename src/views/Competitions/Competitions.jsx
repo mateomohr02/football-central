45418868fff752302247a8aa84e358cff8eb7715
maxi martinatto <martinattomaximiliano@gmail.com>
2023-06-15 23:38:13 -0300

    le di estilos a la view competitions

--- src/views/Competitions/Competitions.jsx
+++ src/views/Competitions/Competitions.jsx
@@ -1,31 +1,35 @@
 import React from 'react'
+import imgArg from '../../assets/banderas/argentina.jpg'
+import imgAle from '../../assets/banderas/alemania.jpg'
+import imgIng from '../../assets/banderas/inglaterra.jpg'
+import imgInt from '../../assets/banderas/internacional.jpeg'
 import style from './Competitions.module.css'
 
 const Competitions = () => {
   return (
     <div className={style.mainContainer}>
-      <div className={style.buttonContainer}>
-        <button className={style.button} type='button'>PAISES</button>
-        <button className={style.button} type='button'>SORTER A-Z</button>
-      </div>
       <div className={style.secondContainer}>
-        <div className={style.containerCountries}>
-          <div>INTERNACIONAL</div>
-          <div>ARGENTINA</div>
-          <div>ALEMANIA</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
-          <div>BANDERA PAIS</div>
+        <div className={style.buttonContainer}>
+          <button className={style.button} type='button'>PAISES</button>
+          <button className={style.button} type='button'>SORTER A-Z</button>
         </div>
-        <div className={style.scroller}></div>
+        <div className={style.containerLeagues}>
+          <div className={style.league}>
+            <img className={style.img} src={imgArg} alt="argentina"/>
+          </div>
+          <div className={style.league}>
+            <img className={style.img} src={imgAle} alt="alemania"/>
+          </div>
+          <div className={style.league}>
+            <img className={style.img} src={imgIng} alt="inglaterra"/>
+          </div>
+          <div className={style.league}>
+            <img className={style.img} src={imgInt} alt="internacional"/>
+          </div>
+          
+        </div>
+       
+        
       </div>
     </div>
   )
