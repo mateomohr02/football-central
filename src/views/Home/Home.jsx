d723eff5223cacfff15dfcc0b97a822f7469bf2a
nicoSaffarano1995 <nico.saffarano@gmail.com>
2023-06-19 15:47:18 -0300

    home-first view -team logos added

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,10 +3,11 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 /* import { getFixtureToday } from "../../redux/actions/getFixtureToday"; */
 import { getFixtureByDateRange } from "../../redux/actions/getFixtureByDateRange";
-
+import NavBar from '../../components/NavBar/NavBar'
 import style from "./Home.module.css";
 import MatchCards from "../../components/MatchCards/MatchCards";
 import News from "../../components/News/News";
+import TeamCards from "../../components/TeamCards/TeamCards";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -17,8 +18,11 @@ const Home = () => {
 
   return (
     <div className={style.mainContainer}>
+      <NavBar/>
       <div className={style.newsContainer}></div>
-      <News/>
+
+      <TeamCards />
+      <News />
       <MatchCards />
     </div>
   );
