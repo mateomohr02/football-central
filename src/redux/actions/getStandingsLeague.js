705d13f52ac316f187e70dce448a60e757838f8d
mateomohr02 <mohr.mateo@gmail.com>
2023-06-19 19:22:21 -0300

    fixed detail league

--- src/redux/actions/getStandingsLeague.js
+++ src/redux/actions/getStandingsLeague.js
@@ -1,40 +1,15 @@
 import axios from "axios";
 
-export function getStandingsLeague (league_id){
-    return async function(dispatch){
+export function getStandingsLeague(id){
+    
+    return async function (dispatch){
+        let apiData = await axios.get(`http://localhost:3001/standings/20873?league_id=${id}`)
         
-        const standings = [];
+        let standings = apiData.data
 
-        if (league_id === 636) {
-            standings = await axios.get(`/league/20837?league_id=${league_id}`)
-        }
-
-        if (league_id === 82) {
-            standings = await axios.get(`/league/20837?league_id=${league_id}`)
-        }
-
-        if (league_id === 85) {
-            standings = await axios.get(`/league/20837?league_id=${league_id}`)
-        }
-
-        if (league_id === 8) {
-            standings = await axios.get(`/league/20837?league_id=${league_id}`)
-        }
-
-        
-        if (!standings) {
-            return({
-                type: 'GET_STANDINGS_LEAGUE',
-                payload: []
-            })
-        }
-        
-        else{
-            return dispatch({
-                type: 'GET_STANDINGS_LEAGUE',
-                payload: standings
-            })   
-        }
-
+        return dispatch({
+            type: 'GET_STANDINGS_LEAGUE',
+            payload: standings
+        })
     }
 }
