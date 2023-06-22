6fb277bc328dd85db1b496a17f59ddfe4302a1a2
mateomohr02 <mohr.mateo@gmail.com>
2023-06-22 18:34:03 -0300

    cambios para deploy

--- src/redux/actions/getPlayerById.js
+++ src/redux/actions/getPlayerById.js
@@ -4,7 +4,7 @@ import { GET_PLAYER_BY_ID } from "./actions-type";
 export const getPlayerById = (id) => {
   return async function (dispatch) {
     try {
-      const response = await axios.get(`http://localhost:3001/players/${id}`);
+      const response = await axios.get(`/players/${id}`);
       const data = response.data;
       dispatch({
         type: GET_PLAYER_BY_ID,
