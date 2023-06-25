073d784d5066a26277a09c16638c7fa0d185af32
maxi martinatto <martinattomaximiliano@gmail.com>
2023-06-25 11:41:05 -0300

    profile componentes y views

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ProfilePost from '../../components/ProfilePost/ProfilePost'
+import ProfileInfo from '../../components/ProfileInfo/ProfileInfo'
+import ProfileChat from '../../components/ProfileChat/ProfileChat'
+import NavBar from '../../components/NavBar/NavBar'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className="bg-pf-grey md:h-screen">
+      <NavBar/>
+      <ProfileInfo/>
+      <ProfilePost/>
+      <ProfileChat/>
+
+    </div>
   )
 }
 
