08406850d3ea50a6e75393cb27852aa896fea4c7
oceanamassano <oshimassano@gmail.com>
2023-06-26 18:48:15 -0300

    added from Branch Alexis

--- src/redux/reducer/teamReducer.js
+++ src/redux/reducer/teamReducer.js
@@ -1,9 +1,12 @@
 import {
+  FILTER_TEAMS,
+  FILTER_TEAMSBYCOUNTRY,
   GET_DETAIL_TEAM,
   GET_TEAM_BY_NAME,
   RESET_DETAIL_TEAM,
   RESET_SHOWED_TEAMS,
   SEARCH_TEAM_LEAGUE,
+  TEAMS,
 } from "../actions/actions-type";
 
 const initialTeamState = {
@@ -11,6 +14,8 @@ const initialTeamState = {
   showedTeams: [],
   detailTeam: [],
   team: [],
+  teams_filter: [],
+  team_filter_combinados: [],
 };
 
 const teamReducer = (state = initialTeamState, action) => {
@@ -32,6 +37,45 @@ const teamReducer = (state = initialTeamState, action) => {
         ...state,
         team: action.payload,
       };
+    case FILTER_TEAMSBYCOUNTRY: {
+      const teams = state.teams;
+      const filterByCountry =
+        action.payload === "All"
+          ? []
+          : teams.filter((team) => team.country_id === action.payload);
+
+      return {
+        ...state,
+        teams_filter_combinados: filterByCountry,
+      };
+    }
+
+    case FILTER_TEAMS: {
+      if (action.payload === "A") {
+        return {
+          ...state,
+          teams_filter: [...state.teams_filter].sort((a, b) =>
+            a.name.localeCompare(b.name)
+          ),
+          teams_filter_combinados: [...state.team_filter_combinados].sort(
+            (a, b) => a.name.localeCompare(b.name)
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          teams_filter: [...state.teams_filter].sort(
+            (a, b) => -1 * a.name.localeCompare(b.name)
+          ),
+          team_filter_combinados: [...state.team_filter_combinados].sort(
+            (a, b) => -1 * a.name.localeCompare(b.name)
+          ),
+        };
+      }
+    }
+
+    case TEAMS:
+      return { ...state, teams_filter: action.payload };
     default:
       return state;
   }
