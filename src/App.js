96e5bbc991bbac59d552f8dff97c4da4108c04c5
PedroMB22 <pedromb.bocco@gmail.com>
2023-06-27 12:59:13 -0300

    profile

--- src/App.js
+++ src/App.js
@@ -9,20 +9,14 @@ import Community from "./views/Community/Community.jsx";
 import Landing from "./views/Landing/Landing.jsx";
 import NavBar from "./components/NavBar/NavBar";
 import DetailLeague from "./views/DetailLeague/DetailLeague.jsx";
-import DetailCup from "./views/DetailCup/DetailCup.jsx";
+import DetailCup from "./views/DetailCup/DetailCup.jsx"
 import CountryCompetitions from "./views/CountryCompetitions/CountryCompetitions";
 import DetailTeam from "./views/DetailTeam/DetailTeam";
 import TeamSearch from "./views/TeamSearch/TeamSearch";
-import NotFound from "./views/404/NotFound";
 import { useEffect } from "react";
-import Premium from "./views/Premium/Premium";
-import Success from "./views/Premium/Success";
-import Profile from "./views/Profile/Profile";
-
 
 function App() {
   const location = useLocation();
-
   useEffect(() => {
     const loggedIn = localStorage.getItem("loggedIn"); // Obtener el valor de loggedIn desde el localStorage
 
@@ -42,31 +36,36 @@ function App() {
       {localStorage.getItem("loggedIn") === "true" ? (
         <NavBar />
       ) : (
-        (location.pathname === "/" || location.pathname === "/register") ? (
-          location.pathname === "/" ? <Landing /> : <Register />
+        (location.pathname === '/' || location.pathname === '/register') ? (
+          location.pathname === '/' ? <Landing /> : <Register />
         ) : (
           // Redirigir al usuario a la página de inicio de sesión
           <Navigate to="/" />
         )
       )}
-
       <Routes>
-        <Route path="/registro" element={<Register />} />
-        <Route path="/inicio" element={<Home />} />
-        <Route path="/comunidad" element={<Community />} />
-        <Route path="/competiciones" element={<Competitions />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/competitions" element={<Competitions />} />
+        <Route exact path="/community" element={<Community />} />
         <Route
-          path="/competitions/countries/:id"
+          exact
+          path="/competitions/country/:id"
           element={<CountryCompetitions />}
         />
-        <Route path="/competitions/leagues/:id" element={<DetailLeague />} />
-        <Route path="/competitions/cups/:id" element={<DetailCup />} />
-        <Route path="/team/:id" element={<DetailTeam />} />
-        <Route path="/search" element={<TeamSearch />} />
-        <Route path="/premium" element={<Premium />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          exact
+          path="/competitions/league/:id"
+          element={<DetailLeague />}
+        />
+        <Route
+          exact
+          path="/competitions/cup/:id"
+          element={<DetailCup />}
+        />  
+        <Route exact path="/team/:id" element={<DetailTeam/>}/>
+
+        <Route exact path="/search" element={<TeamSearch />}/>
       </Routes>
     </div>
   );
