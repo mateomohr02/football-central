00b0265a10ad9efdf55eb66c527cd51426a532f6
nicoSaffarano1995 <nico.saffarano@gmail.com>
2023-06-26 21:54:36 -0300

    team logos succesfully shown

--- src/components/TeamCard/TeamCard.jsx
+++ src/components/TeamCard/TeamCard.jsx
@@ -1,7 +1,14 @@
-import style from './TeamCard.module.css'
+import { Link } from "react-router-dom";
+import style from "./TeamCard.module.css";
 
-const TeamCard = ({name,logo}) => {
-  return <div className={style.cardContainer}><img src={logo} alt={name} className={style.img} /></div>;
+const TeamCard = ({KEY, name, logo }) => {
+  return (
+    <div className={style.cardContainer}>
+      <Link to="/">
+        <img src={logo} alt={name} className={style.img} />
+      </Link>
+    </div>
+  );
 };
 
 export default TeamCard;
