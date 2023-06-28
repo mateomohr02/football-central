6a742f3ebb48fa9a5a2ddafd2290f5dea936da9c
oceanamassano <oshimassano@gmail.com>
2023-06-28 19:44:03 -0300

    profile pic changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -27,13 +27,17 @@ const Navbar = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-
+  const image = useSelector((state) => state.user.userProfile.image.imageUrl);
   return (
     <nav className="relative bg-white shadow dark:bg-gray-800">
       <div className="container py-3 mx-auto md:flex">
         <div className="flex items-center justify-between">
           <Link to="/inicio">
-            <img className="w-auto h-9 pl-3 sm:pl-0 sm:h-12" src={logo} alt="" />
+            <img
+              className="w-auto h-9 pl-3 sm:pl-0 sm:h-12"
+              src={logo}
+              alt=""
+            />
           </Link>
 
           {/* Mobile menu button */}
@@ -53,7 +57,11 @@ const Navbar = () => {
                   stroke="currentColor"
                   strokeWidth="2"
                 >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 8h16M4 16h16"
+                  />
                 </svg>
               )}
 
@@ -66,7 +74,11 @@ const Navbar = () => {
                   stroke="currentColor"
                   strokeWidth="2"
                 >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
                 </svg>
               )}
             </button>
@@ -82,8 +94,18 @@ const Navbar = () => {
           <div className="flex w-full items-end flex-col pr-2 mx-4 md:flex-row md:justify-end md:items-center md:mr-6 md:py-0">
             {isAuthenticated ? (
               <>
-                <Link to="/profile" className="h-12 w-12 flex justify-center items-center sm:absolute -right-6">
-                  <AccountCircleIcon fontSize="large" className="text-pf-white text-2xl" />
+                <Link
+                  to="/profile"
+                  className="h-12 w-12 flex justify-center items-center sm:absolute -right-6"
+                >
+                  {image !== "" ? (
+                    <img src={image} alt="ProfilePicture" style={{ borderRadius: '50%', width: '100%', height: '100%' }}/>
+                  ) : (
+                    <AccountCircleIcon
+                      fontSize="large"
+                      className="text-pf-white text-2xl"
+                    />
+                  )}
                 </Link>
                 <button
                   onClick={handleLogout}
@@ -124,7 +146,11 @@ const Navbar = () => {
 
             <div className="relative mt-4 md:mt-0 pr-2">
               <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
+                <svg
+                  className="w-5 h-5 text-gray-400"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
                   <path
                     d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
                     stroke="currentColor"
