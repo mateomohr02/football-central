a119b919454724c33a5477c31ee0e73014766f9c
oceanamassano <oshimassano@gmail.com>
2023-06-29 19:12:28 -0300

    took out import

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProfile } from "../../redux/actions/getProfile";
-import { updateUserImage } from "../../redux/actions/updateUserImage";
 import { Link } from "react-router-dom";
 import StarsIcon from "@mui/icons-material/Stars";
 import axios from "axios";
