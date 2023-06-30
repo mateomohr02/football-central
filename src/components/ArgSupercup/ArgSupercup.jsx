b286bb0b0de1c306e3b30c2f154d1df2677067d3
mateomohr02 <mohr.mateo@gmail.com>
2023-06-29 22:24:52 -0300

    'ger cup done'

--- src/components/ArgSupercup/ArgSupercup.jsx
+++ src/components/ArgSupercup/ArgSupercup.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getArgSupercup } from '../../redux/actions/getArgSupercup'
+import { resetDetailCup } from '../../redux/actions/resetDetailCup'
 
 const ArgSupercup = () => {
 
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getArgSupercup())
+    return () => resetDetailCup()
+  }, [dispatch])
+
+  const final = useSelector(state => state.leagueCup.detailCup)
+
   return (
-    <div>ArgSupercup</div>
+    <div>
+      <h3>Final</h3>
+      <div>
+        <span><img src={final?.boca?.image_path} alt={final?.boca?.name} />{final?.name}<img src={final?.patronato?.image_path} alt={final?.patronato?.name}></img></span>
+        <span>{final?.finalResult}</span>
+      </div>
+    </div>
   )
 }
 
--- src/components/GerDBFPokal/GerDBFPokal.jsx
+++ src/components/GerDBFPokal/GerDBFPokal.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getGerDBFPokal } from '../../redux/actions/getGERDBFPokal'
+import { resetDetailCup } from '../../redux/actions/resetDetailCup'
+import PlayOffMatchCard from '../PlayOffMatchCard/PlayOffMatchCard';
+
 
 const GerDBFPokal = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getGerDBFPokal())
+    return () => resetDetailCup()
+  }, [dispatch])
+
+  const detailCup = useSelector(state => state.leagueCup.detailCup)
+
   return (
-    <div>GerDBFPokal</div>
+    <div>
+      {/* OCTAVOS */}
+    <div>
+    <h3>Octavos de Final</h3>
+      <PlayOffMatchCard
+      arrMatches={detailCup?.quarterFinals}
+      />
+    </div>
+    {/* CUARTOS */}
+    <div>
+    <h3>Cuarts de Final</h3>
+      <PlayOffMatchCard
+      arrMatches={detailCup?.quarterFinals}
+      />
+    </div>
+    {/* SEMIFINAL */}
+    <div>
+    <h3>Semi-Final</h3>
+      <PlayOffMatchCard
+      arrMatches={detailCup?.semiFinal}
+      />
+    </div>
+    {/* FINAL */}
+    <div>
+      <h3>Final</h3>
+      <PlayOffMatchCard
+      arrMatches={detailCup?.final}
+      />
+    </div>
+    </div>
   )
 }
 
