44046f08eed4646c109071c50893a216c9f196ce
Alexis Michia <alexi890@outlook.com>
2023-06-30 23:02:53 -0300

    store al 30%?

--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -9,6 +9,8 @@ import venueReducer from "./venueReducer";
 import playerReducer from "./playerReducer";
 import newsReducer from "./newsReducer";
 import reviewsReducer from "./reviewsReducer"
+import storeReducer from "./storeReducer"
+
 //all reducers get sent dispatch, only one acts on it
 const rootReducer = combineReducers({
   team: teamReducer,
@@ -21,6 +23,7 @@ const rootReducer = combineReducers({
   player: playerReducer,
   news: newsReducer,
   reviews: reviewsReducer,
+  store: storeReducer,
 });
 
 export default rootReducer;
---
+++ src/redux/reducer/storeReducer.js
@@ -0,0 +1,35 @@
+import {
+    GET_PRODUCTS,
+    GET_PRODUCTSID,
+    CREATE_PRODUCT,
+    
+  } from '../actions/actions-type';
+  
+  const initialState = {
+    products: [],
+    product: {},
+    cratesProducts: [],
+    cart: [],
+    loading: false,
+    error: null
+  };
+  
+  const storeReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_PRODUCTS:
+      return { ...state, products: action.payload };
+      case GET_PRODUCTSID:
+      return { ...state, product: action.payload };
+      case CREATE_PRODUCT:
+      return { ...state, createsProducts: action.payload };
+      
+      
+      
+     
+      default:
+        return state;
+    }
+  };
+  
+  export default storeReducer;
+  
