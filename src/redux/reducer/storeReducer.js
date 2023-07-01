76fbb9d15960acba4715080ad4cc27eefc7a3ba6
mateomohr02 <mohr.mateo@gmail.com>
2023-07-01 14:30:49 -0300

    pre merge

--- src/redux/reducer/storeReducer.js
+++ src/redux/reducer/storeReducer.js
@@ -1,16 +1,27 @@
 const intialStoreState = {
     products: [],
+    newProduct: false
   };
   
   const storeReducer = (state = intialStoreState, action) => {
     switch (action.type) {
-      case 'GET_ALL_PRODUCTS':
-        return{
-            ...state,
-            products : action.payload
-        }
-      default:
-        return state;
+        case 'GET_ALL_PRODUCTS':
+            return{
+                ...state,
+                products : action.payload
+            }
+        case 'POST_NEW_PRODUCT':
+            return{
+                ...state,
+                newProduct: true
+            }
+        case 'RESET_NEW_PRODUCT':
+            return{
+                ...state,
+                newProduct: false
+            }
+        default:
+            return state;
     }
   };
   
--- src/views/AdminStore/AdminStore.jsx
+++ src/views/AdminStore/AdminStore.jsx
@@ -1,20 +1,68 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-
+import { postProduct } from '../../redux/actions/postProduct'
+import { resetNewProduct } from '../../redux/actions/resetNewProduct'
 
 const AdminStore = () => {
 
     const navigate = useNavigate()
-
+    const dispatch = useDispatch()
     const user = useSelector(state => state.user.userProfile)
 
-    if (user.role !== 'admin'){
-        navigate('/404')
+    // if (user.role !== 'admin'){
+    //     navigate('/404')
+    // }
+    const allProduts = useSelector(state => state.store.products)
+
+    const[newProduct, setNewProduct] = useState({})
+
+    const handleChange = (e) => {
+        setNewProduct(e.target.name = e.target.value)
+        console.log(newProduct);
     }
 
-  return (
-    <div>AdminStore</div>
+    const handleSubmit = (e) => {
+        console.log(newProduct);
+        //dispatch(postProduct())
+    }
+
+    useEffect(()=>{
+        return () => dispatch(resetNewProduct())
+    }, [dispatch])
+
+    return (
+      <div>
+          {allProduts ? allProduts.map(p => {
+              return (
+                  <div>
+                      Producto
+                  </div>
+              )
+          }): <span>No existen productos</span>}
+          <br />
+          <h3>Nuevo Producto</h3>
+          <form>
+            <input type="text" value={newProduct.name} name='name' onChange={handleChange}/>
+            <br />
+            <input type="text" value={newProduct.price} name='price' />
+            <br />
+            <input type="text" value={newProduct.description} name='description' />
+            <br />
+            <input type="text" value={newProduct.sku} name='sku' />
+            <br />
+            <input type="text" value={newProduct.stock} name='stock' />
+            <br />
+            <input type="text" value={newProduct.category} name='category' />
+            <br />
+            <input type="text" value={newProduct.brand} name='brand' />
+            <br />
+            <input type="text" value={newProduct.image_path} name='image_path' />
+            <br />
+            
+            <button onClick={() => handleSubmit}>PUBLICAR</button>
+          </form>
+        </div>
   )
 }
 
